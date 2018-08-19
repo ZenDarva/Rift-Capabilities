a9724487a340ffbc3644b67576c7bd485f6075a9
Darva <zendarva@gmail.com>
2018-08-19 18:27:00 -0500

    The rest of the containers.

--- src/main/java/com/gmail/zendarva/capabilities/mixins/IInventoryMixin.java
+++ src/main/java/com/gmail/zendarva/capabilities/mixins/IInventoryMixin.java
@@ -6,16 +6,13 @@ import com.gmail.zendarva.capabilities.API.ICapabilityProvider;
 import com.gmail.zendarva.capabilities.items.IItemHandler;
 import com.gmail.zendarva.capabilities.items.VanillaInventoryWrapper;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.tileentity.TileEntityChest;
-import net.minecraft.tileentity.TileEntityDispenser;
-import net.minecraft.tileentity.TileEntityDropper;
-import net.minecraft.tileentity.TileEntityHopper;
+import net.minecraft.tileentity.*;
 import net.minecraft.util.EnumFacing;
 import org.spongepowered.asm.mixin.Mixin;
 
 import java.util.Optional;
 
-@Mixin({TileEntityChest.class, TileEntityDispenser.class, TileEntityDropper.class, TileEntityHopper.class})
+@Mixin({TileEntityChest.class, TileEntityDispenser.class, TileEntityDropper.class, TileEntityHopper.class, TileEntityShulkerBox.class,TileEntityJukebox.class, TileEntityTrappedChest.class})
 public class IInventoryMixin implements ICapabilityProvider {
 
     VanillaInventoryWrapper wrapper = null;
