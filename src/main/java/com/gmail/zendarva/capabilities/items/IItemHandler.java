e3dcc0e403170977b1b38a2705f131abc948ca8e
Darva <zendarva@gmail.com>
2018-08-19 15:34:46 -0500

    Comment.

--- src/main/java/com/gmail/zendarva/capabilities/items/IItemHandler.java
+++ src/main/java/com/gmail/zendarva/capabilities/items/IItemHandler.java
@@ -6,6 +6,11 @@ import net.minecraft.nbt.NBTTagCompound;
 
 public interface IItemHandler extends ICapability, IInventory {
 
+    /*These methods are for implementing transactionality.
+    After you take an item from a slot, you may lock the slot, which will prevent any operation from using that slot.
+    Once it has reached its destination (Which, presumably, you have also locked), the slot may be unlocked.
+    If the transfer fails, you would return the item to this inventory, unlock the slot, and replace the ItemStack.
+    */
     public void lockSlot(int slot);
     public void unlockSlot(int slot);
     public boolean isSlotLocked(int slot);
